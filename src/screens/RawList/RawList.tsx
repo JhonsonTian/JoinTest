fab3c4549dcaef35ecc3251a9c0dc9c25ac069ba
Jhonson Tian <jhonson.tian@yahoo.com>
2020-12-21 11:20:32 +0700

    feat: filter by material name

--- src/screens/RawList/RawList.tsx
+++ src/screens/RawList/RawList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { View, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { ListItem, Button } from 'react-native-elements';
+import { ListItem, Button, Input } from 'react-native-elements';
 import { getRawMaterial } from 'src/api';
 import { RawMaterial } from 'src/api/types';
 import { Loading } from 'src/components/Loading';
@@ -17,8 +17,17 @@ type Props = {
 export const RawList: React.FC<Props> = ({ navigation }) => {
   const [page, setPage] = useState(1);
   const [selectedId, setSelectedId] = useState<number[]>([]);
+  const [search, setSearch] = useState('');
   const [isLoading, setLoading] = useState(true);
   const [materialList, setMaterialList] = useState<RawMaterial[]>([]);
+
+  const filterText = new RegExp(search, 'i');
+
+  let filteredList = materialList;
+
+  if (search !== '') {
+    filteredList = materialList.filter(item => filterText.test(item.nameEng));
+  }
 
   const onItemPress = (id: number) => () => {
     const isIdSelected = selectedId.some(item => item === id);
@@ -46,12 +55,16 @@ export const RawList: React.FC<Props> = ({ navigation }) => {
   };
 
   const onLoadMore = () => {
-    setLoading(true);
-    setPage(prevPage => prevPage + 1);
+    if (search === '') {
+      setLoading(true);
+      setPage(prevPage => prevPage + 1);
+    }
   };
 
+  const onChangeText = (value: string) => setSearch(value);
+
   const renderFooter = () => {
-    if (isLoading && materialList.length !== 0) {
+    if (isLoading && materialList.length !== 0 && search.length === 0) {
       return (
         <ActivityIndicator
           size="small"
@@ -79,9 +92,17 @@ export const RawList: React.FC<Props> = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <Input
+        placeholder="Material Name"
+        containerStyle={styles.inputContainer}
+        inputContainerStyle={styles.input}
+        value={search}
+        onChangeText={onChangeText}
+      />
       <View style={styles.virtualList}>
         <FlatList
-          data={materialList}
+          data={filteredList}
+          // extraData={materialList.length}
           renderItem={onRenderList}
           onEndReachedThreshold={0.2}
           onEndReached={onLoadMore}
--- src/screens/RawList/styles.ts
+++ src/screens/RawList/styles.ts
@@ -14,4 +14,17 @@ export const styles = StyleSheet.create({
   },
   activityIndicator: { height: 50 },
   virtualList: { flex: 1 },
+  inputContainer: {
+    width: '90%',
+    height: 45,
+    borderWidth: 1,
+    borderRadius: 10,
+    marginVertical: 15,
+    alignSelf: 'center',
+  },
+  input: {
+    width: '100%',
+    height: 45,
+    borderBottomWidth: 0,
+  },
 });
